d3474ed0369cbff4251809123ab98eb4094fa284
adityatiple <atipl367@mtroyal.ca>
2025-03-07 17:55:31 -0700

    created input.c and input.h for reading input

---
+++ input.c
@@ -0,0 +1,12 @@
+#include <osbind.h>
+#include "input.h"
+
+/* Check if input is pending */
+int input_pending() {
+    return Cconis();
+}
+
+/* Read input */
+char read_input() {
+    return (char)Cnecin();
+}
---
+++ input.h
@@ -0,0 +1,12 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include "model.h"
+
+/* Function to check if input is pending */
+int input_pending();
+
+/* Function to read input */
+char read_input();
+
+#endif /* INPUT_H */
